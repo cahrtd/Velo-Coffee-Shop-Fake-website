cbd73f77246879cd3f528a4d30670c7548a449e7
Milena <milenatymofiichuk@gmail.com>
2025-12-01 20:25:28 -0700

    Edited Navbar and Hero Section styles and structure. Added JS file.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// CHANGE LATER WHEN SCROLLED
+// const navbar = document.getElementById("nav-container");
+// const scrollThreshold = 50; // pixels scrolled before change
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > scrollThreshold) {
+//     navbar.classList.add("scrolled");
+//   } else {
+//     navbar.classList.remove("scrolled");
+//   }
+// });
